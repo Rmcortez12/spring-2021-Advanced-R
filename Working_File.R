d92393fe82676a81e5d4a36eebaeedafbec6419c
Rmcortez12 <33045009+Rmcortez12@users.noreply.github.com>
2021-03-14 21:53:19 -0500

    updating

--- Working_File.R
+++ Working_File.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(plyr)
 library(stringr)
 library(data.table)
+install.packages("xlsx")
+library("xlsx")
 
 #URL: 
 #this url actually has almost all the data we need
@@ -15,7 +17,7 @@ library(data.table)
 
 #create an empty data frame to populate via loop
 df <- data.frame(matrix(ncol = 11, nrow = 0))
-x <- c("Year","VehicleName","WD","Turbo","EngineSize","NumCyl","Transmission","FuelType","MpgCombined", "MpgCity","MpgHwy")
+x <- c("Year","VehicleName","WD","EngineMod","EngineSize","NumCyl","Transmission","FuelType","MpgCombined", "MpgCity","MpgHwy")
 colnames(df) <- x
 
 
@@ -36,16 +38,23 @@ get_car_sub_details <- function(x){
 #Begin building out the beginning of the car entry to the dataframe
 build_car_entry <- function(x){
   car_details = strip_spec_chars(x)
-  #[yearnamewheel,enginesize,cylinders,transmission,optional(turbo),type]
+  #[yearnamewheel,enginesize,cylinders,transmission,optional(turbo/supercharged),type]
   #print(paste0("Working on ", car_details[1]))
   sub_car_details = get_car_sub_details(car_details)
   #[year,name,wheeldrive]
   if(length(car_details)==6&&car_details[5]=="Turbo"){
     sub_car_details<- c(sub_car_details,"1")
     car_details <- car_details[-5]
-  }else{
+  }else if (length(car_details)==6 &&car_details[5]=="Supercharger"){
+    sub_car_details<- c(sub_car_details,"2")
+    car_details <- car_details[-5]
+  } else{
     sub_car_details<- c(sub_car_details,"0")
   }
+  if(!str_detect(car_details[3],"cyl")){
+    car_details <- c(car_details[1:2],"2cyl",car_details[3:length(car_details)])
+  }
+  
   car_details <- car_details[-1] #remove the first element because we split it up earlier
   car_details <- c(sub_car_details,car_details)
   return(car_details)
@@ -89,6 +98,7 @@ web_scraper <- function(x){
       curr_car = df_new[1,1]
       curr_df_index = 1
       car_dets <- build_car_entry(curr_car)
+      #print(car_dets)
       #[year,name,wd,class(turbo),engine,cyl,transmission,gastype]
       df[curr_df_index,]<-c(car_dets,"MpgCombined","City","HWY")
     }
@@ -100,6 +110,7 @@ web_scraper <- function(x){
       curr_df_index = curr_df_index+1
       curr_car = df_new[i,1]
       car_dets <- build_car_entry(curr_car)
+      #print(car_dets)
       df[curr_df_index,] <- c(car_dets,"MpgCombined","City","HWY")
       
     }
@@ -127,13 +138,13 @@ web_scraper <- function(x){
 }
 
 #there are 111 pages
-base_url <- "https://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year1=2000&year2=2022&cbmkJeep=Jeep&minmsrpsel=0&maxmsrpsel=0&city=0&hwy=0&comb=0&cbvtgasoline=Gasoline&YearSel=2000-2022&make=&mclass=&vfuel=&vtype=Gasoline&trany=&drive=&cyl=&MpgSel=000&sortBy=&Units=&url=SearchServlet&opt=new&minmsrp=0&maxmsrp=0&minmpg=0&maxmpg=0&sCharge=&tCharge=&startstop=&cylDeact=&rowLimit=200&pageno="
+base_url <- "https://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&year1=1976&year2=2022&cbmkJeep=Jeep&minmsrpsel=0&maxmsrpsel=0&city=0&hwy=0&comb=0&cbvtgasoline=Gasoline&YearSel=2000-2022&make=&mclass=&vfuel=&vtype=Gasoline&trany=&drive=&cyl=&MpgSel=000&sortBy=&Units=&url=SearchServlet&opt=new&minmsrp=0&maxmsrp=0&minmpg=0&maxmpg=0&sCharge=&tCharge=&startstop=&cylDeact=&rowLimit=200&pageno="
 
 init_url <- paste0(base_url,1)
 
 data <- web_scraper(init_url)
 
-for(i in 2:111){
+for(i in 2:197){
   x <- paste0(base_url,i)
   print(paste0("working on page ",i)) 
   data <- rbind(data,web_scraper(x))
@@ -211,3 +222,52 @@ unique(sort(test_data$VehicleMake))
 
 ford_Df = test_data[which(test_data$VehicleMake=="Ford"),]
 
+#write data to a csv so we don't have to re-pull every time
+write.csv(test_data,file = "webdata.csv")
+
+
+#Retrieve data from wikipedia for CAFE Standards
+wiki <- read_html("https://en.wikipedia.org/wiki/Corporate_average_fuel_economy#:~:text=The%20program%20covered%20model%20year,of%2025%20miles%20per%20gallon.")
+wiki_table = html_table(html_nodes(wiki,"table"),header = T, fill = T)
+wiki_df = data.frame(wiki_table[3])
+names(wiki_df)
+wiki_names <- c('Year','PassengerDomCars','PassengerImportCars','LightTrucks2wd','LightTrucks4wd','LightTrucksCombined')
+names(wiki_df)<-wiki_names
+wiki_df <-wiki_df[2:41,]
+wiki_df[,1]<- sapply(wiki_df[,1],function(x){substr(x,1,4)}) # need to get rid of special characters and just keep years
+
+#problem classifying cars vs trucks vs vans
+
+#Found better data set at https://afdc.energy.gov/data/10562 for cafe
+cafe_df <- read.xlsx2('cafe.xlsx',2,header = T)
+detach("package:xlsx", unload = TRUE)
+plot(as.character(cafe_df$Year),as.character(cafe_df$Passenger),type="l",xlab = "Year", ylab = "CAFE MPG Standard")
+points(as.character(cafe_df$Year),as.character(cafe_df$LightTrucks), type = "l",col="red")
+points(ford_Df$Year,ford_Df$MpgCombined)
+grid(NULL,NULL,lty = 6)
+
+#need to get mean year mpg for all brands
+
+make_stats <- data.frame()
+df_make <- data.frame()
+
+for(make in unique(sort(test_data$VehicleMake))){
+  df_make <- test_data[which(test_data$VehicleMake==make),]
+  for(year in unique(sort(df_make$Year))){
+    d <- as.numeric(df_make$MpgCombined[df_make$Year==year])
+    make_stats <- rbind(make_stats,data.frame(year,make,mean(d),var(d),median(d)))
+  }
+}
+
+make_stats$year <- as.character(make_stats$year)
+sort_data_year <- make_stats[order(as.character(make_stats$year)),]
+plot(as.factor(sort_data_year$year),sort_data_year$mean.d.,ylab = "Combined MPG",xlab = "Year")
+plot(sort_data_year[which(sort_data_year$make == "Acura"),]$year,sort_data_year[which(sort_data_year$make == "Acura"),]$mean.d.,ylab = "Combined MPG",xlab = "Year", main="Acura",type="l")
+
+plot(sort_data_year[which(sort_data_year$make == "Ford"),]$year,sort_data_year[which(sort_data_year$make == "Ford"),]$mean.d.,ylab = "Combined MPG",xlab = "Year", main="Ford",type="l")
+
+
+
+#enhancements
+#get number of cars sold
+#break into categories for cars/trucks/suv's
